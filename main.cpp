01daf20c477e9f186964bb6a3abe50a12c21ffa3
HaoChang Fu <fuhaochang664961423@hotmail.com>
2022-10-27 22:40:32 +0100

    changed processingTool to a interface

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include "Headers/CSVReader.h"
-#include "Headers/ProcessingTool.h"
+#include "Headers/Interface.h"
 using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     CSVReader reader;
-    /*reader.populate();*/
-    int userInput;
-    ProcessingTool interface;
-    UCTurma ucturma= *(reader.getUcTurmas().begin());
-    cout<<ucturma.get_uc_turma().first<<endl;
+    //reader.populate();
+    string userInput;
+    Interface interface;
+    /*UCTurma ucturma= *(reader.getUcTurmas().begin());
+    cout<<ucturma.get_uc_turma().first<<endl*/
     while(true){
         cout<<"Introduza o numero do comando:\n\t1.Listagem\n\t2.Alteracoes\n\t0.Sair do programa"<<endl;
         cin>>userInput;
-        if (userInput==0){
+        if (userInput=="0"){
             break;
         }
         interface.listShow(userInput);
