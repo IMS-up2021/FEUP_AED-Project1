5a72e929903eef215d5c4de4cae87ec14a2fc918
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-06 18:44:26 +0000

    Updated documentation

--- docs/output/html/search/all_b.js
+++ docs/output/html/search/all_b.js
@@ -5,6 +5,5 @@ var searchData=
   ['set_5ftimetable_2',['set_timetable',['../class_student.html#a3367e4af0a86234a10a9ca9577ef071e',1,'Student']]],
   ['slot_3',['Slot',['../class_slot.html',1,'Slot'],['../class_slot.html#a5e80cc94a5aae704be5f8dd7e4e3f5ae',1,'Slot::Slot()']]],
   ['student_4',['Student',['../class_student.html',1,'Student'],['../class_student.html#a160c7f7b10b41ee6131af78ce4d67366',1,'Student::Student(string name, unsigned num)'],['../class_student.html#a72487ab67fbbc85ed15d7f59df197c57',1,'Student::Student(unsigned num)'],['../class_student.html#af9168cedbfa5565cf0b20c1a9d3f5c9d',1,'Student::Student()']]],
-  ['student_5fnum_5fgood_5',['student_num_good',['../class_u_c_turma.html#a60c2535c38a8671a6a6e41f426ba790a',1,'UCTurma']]],
-  ['studentptr_6',['StudentPtr',['../class_student_ptr.html',1,'']]]
+  ['student_5fnum_5fgood_5',['student_num_good',['../class_u_c_turma.html#a60c2535c38a8671a6a6e41f426ba790a',1,'UCTurma']]]
 ];
--- docs/output/html/search/classes_3.js
+++ docs/output/html/search/classes_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['slot_0',['Slot',['../class_slot.html',1,'']]],
-  ['student_1',['Student',['../class_student.html',1,'']]],
-  ['studentptr_2',['StudentPtr',['../class_student_ptr.html',1,'']]]
+  ['student_1',['Student',['../class_student.html',1,'']]]
 ];
